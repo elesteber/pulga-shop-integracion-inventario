1f3ce52b9a008bcf79b7469c4c55b28a4f14dc09
Catrilao <87039539+Catrilao@users.noreply.github.com>
2025-12-10 01:18:55 -0300

    feat: agregar usuario de pruebas

--- pulga-shop-inventario-frontend/src/components/AuthRoutes.tsx
+++ pulga-shop-inventario-frontend/src/components/AuthRoutes.tsx
@@ -1,6 +1,11 @@
 import AppLayout from "../layouts/app/AppLayout";
 
 export default function AuthRoutes() {
+  if (import.meta.env.DEV && !localStorage.getItem("jwt")) {
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJWRU5EXzAwMSIsImVtYWlsIjoiY29ycmVvQGNvcnJlby5jb20iLCJyb2xlIjoidmVuZGVkb3IiLCJpYXQiOjE3NjUzMzg3Mzd9.P4VCqqRqmqCwj6EyAzEpn_xMw-rFH4LUDg8CMSLxOk";
+    localStorage.setItem("jwt", token);
+  }
+
   const isAuthenticated = Boolean(localStorage.getItem("jwt"));
 
   if (!isAuthenticated) {
